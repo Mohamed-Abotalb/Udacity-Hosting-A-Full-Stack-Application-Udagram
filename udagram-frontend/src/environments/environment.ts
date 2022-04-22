4689149f63688d3bfbc70919f399d186419ca45b
Mohamed-Abotalb <mohamedabotalb276@gmail.com>
2022-04-22 13:12:23 +0200

    update enviroment code

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'localhost:8081/api/v0'
+  apiHost: 'http://udagram-app-env.eba-mtspkb6b.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
