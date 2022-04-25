ebbf9caab84a47c71a4df9301ea5fea405e140aa
Mohamed-Abotalb <mohamedabotalb276@gmail.com>
2022-04-26 01:19:40 +0200

    update config.yml file

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,5 +1,1 @@
-aws configure --profile "mohamed"
-AWS Access Key ID [None] $AWS_ACCESS_KEY_ID
-AWS Secret Access Key [None]: $AWS_SECRET_ACCESS_KEY
-AWS Secret Region [None]: $AWS_REGION
 aws s3 cp --recursive --acl public-read ./www s3://udagram-mohamed
